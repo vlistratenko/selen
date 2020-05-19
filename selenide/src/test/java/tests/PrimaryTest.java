7cff6c5ce87442d8e8296140b97188c600eda478
listratenko <listratenko@insart.com>
2020-05-19 12:29:35 +0300

    Add browser name and version

--- selenide/src/test/java/tests/PrimaryTest.java
+++ selenide/src/test/java/tests/PrimaryTest.java
@@ -1,9 +1,17 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 
 public class PrimaryTest {
+
+    @BeforeAll
+    public void before() {
+        Configuration.browserCapabilities.setBrowserName("chrome");
+        Configuration.browserCapabilities.setVersion("81");
+    }
 
     @Test
     public void loginTest() {
